9b27c8c0b21a09ff02d0d05ff319ca6e9e180bd2
tgalen <tgalen84@gmail.com>
2022-04-25 11:27:46 -0400

    add filet mignon

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -14,14 +14,20 @@ const CardContainer = ({ isEnglish }) => {
     {
       name: { english: "Chicken Parmagiana", spanish: "Pollo Parmesano" },
       price: { english: "$20", spanish: "400" },
-      type: { english: "Meat, Chicken", spanish: "Carne, Pollo" },
+      type: { english: "White Meat, Chicken", spanish: "Carne, Pollo" },
       image: IMAGES.chickenParm,
     },
     {
       name: { english: "Chicken Marsala", spanish: "Pollo Marsalla" },
       price: { english: "$20", spanish: "400" },
-      type: { english: "Meat, Chicken", spanish: "Carne, Pollo" },
+      type: { english: "White Meat, Chicken", spanish: "Carne, Pollo" },
       image: IMAGES.chickenMarsala,
+    },
+    {
+      name: { english: "Filet Mignon", spanish: "Filete Mignon" },
+      price: { english: "$40", spanish: "800" },
+      type: { english: "Red Meat, Beef", spanish: "Carne Rojo, Carne de Vaca" },
+      image: IMAGES.filetMignon,
     },
   ];
 
